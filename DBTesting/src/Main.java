5b62adfb59fc429c325d4b677f93a7b4f9ab394c
Danny Glover <dannyglover@outlook.com>
2013-07-09 12:45:07 +0800

    Fixed DBTesting
    Now can successfuly connect to mySQL and return password

--- DBTesting/src/Main.java
+++ DBTesting/src/Main.java
@@ -5,63 +5,65 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 
-
 public class Main {
 
-	public static void main(String[] args){
-		String inputPass = "abc123";
-		String user = "Danny";
-		String dbPass=null;
+	public static void main(String[] args) {
+		String inputPass = "abc456";
+		String user = "Brian";
+		String dbPass = null;
 		try {
 			dbPass = idCheck(user);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
 		}
-		//if(inputPass.compareTo(dbPass)==0)
+		if (inputPass.compareTo(dbPass) == 0)
 			System.out.println("It actually worked.");
-		
-	}
-	private static String idCheck(String username) throws SQLException{
-	
-	
-	    Connection dbConnection = null;
-	    PreparedStatement preparedStatement = null;
-	    String pass = null;
-	    String selectSQL = "SELECT Password FROM Players WHERE Username = ?";
-	 
-	    try {
-	      dbConnection = getConnection();
-	      preparedStatement = (PreparedStatement) dbConnection.prepareStatement(selectSQL);
-	      preparedStatement.setString(1, username);
-	 
-	      // execute select SQL statement
-	      ResultSet rs = preparedStatement.executeQuery();
-	 
-	      while (rs.next()) {
-	 
-	        pass = rs.getString("Password");
-	        System.out.println("pass in DB : " + pass);
-	       
-	      }
-	    } catch (SQLException e) {
-	 
-	      System.out.println(e.getMessage());
-	 
-	    } finally {
-	 
-	      if (preparedStatement != null) {
-	        preparedStatement.close();
-	      }
-	 
-	      if (dbConnection != null) {
-	        dbConnection.close();
-	      }
-	      return pass;
-	    }
+		else {
+			System.out.println("Password does not match");
+		}
+
 	}
 
-	
+	private static String idCheck(String username) throws SQLException {
+
+		Connection dbConnection = null;
+		PreparedStatement preparedStatement = null;
+		String pass = null;
+		String selectSQL = "SELECT Password FROM Players WHERE Username = ?";
+
+		try {
+			dbConnection = getConnection();
+			preparedStatement = (PreparedStatement) dbConnection
+					.prepareStatement(selectSQL);
+			preparedStatement.setString(1, username);
+
+			// execute select SQL statement
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+
+				pass = rs.getString("Password");
+				System.out.println("pass in DB : " + pass);
+
+			}
+		} catch (SQLException e) {
+
+			System.out.println(e.getMessage());
+
+		} finally {
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+
+			if (dbConnection != null) {
+				dbConnection.close();
+			}
+			return pass;
+		}
+	}
+
 	public static Connection getConnection() {
 		Connection con = null;
 		try {
@@ -74,5 +76,5 @@ public class Main {
 			System.out.println(ex.getMessage());
 		}
 		return con;
-	}	
+	}
 }
