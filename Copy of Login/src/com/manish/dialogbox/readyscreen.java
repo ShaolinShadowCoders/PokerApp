fc3364836d668da41365cae7816bd3a6503be208
Tom Weidmaier <tommyweid911@gmail.com>
2013-07-10 14:50:25 +0800

    Added Copy of Login, working to game screen

---
+++ Copy of Login/src/com/manish/dialogbox/readyscreen.java
@@ -0,0 +1,27 @@
+package com.manish.dialogbox;
+//this is to save. boo yah
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class readyscreen extends Activity {
+
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.readyscreen);
+	        
+	        Button mainNext = (Button) findViewById(R.id.readyBtn);
+	        mainNext.setOnClickListener(new OnClickListener() {
+	            public void onClick(View v) {
+	            	String ready = "True";
+	                Intent j = new Intent(v.getContext(), gamescreen.class);
+//	                j.setClassName("com.screenssample", "com.screenssample.gamescreen");
+	                startActivity(j);
+	            }
+	        });
+	    }
+	 
+}
